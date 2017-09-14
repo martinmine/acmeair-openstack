e7c9fbb69f4273a092fc35ebcaea265a302ff304
martinmine <martin_mine@hotmail.com>
2017-09-14 16:02:01 +0200

    Added script for spawning servers

---
+++ spawn-servers.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -e
+
+source MACS490_H17_martisny-openrc.sh
+imageID=$(openstack image list --property name="CentOS 7 x86_64" -c ID -f value)
+echo "Using image $imageID"
+networkID=$(openstack network list -f value | grep "acmeair-network" | awk -F ' ' '{print $1}')
+echo "Using network $networkID"
+keyName='skyhigh-martisny'
+rm servers
+
+while IFS= read -r serverHostname
+do
+    serverId=''
+
+    echo "Attempting to spawn instance $serverHostname with image ID $imageID and network ID $networkID"
+    openstack server create --image $imageID --flavor m1.medium --security-group default --key-name $keyName --nic net-id=$networkID $serverHostname
+
+    while [[ $serverId == '' ]]
+    do
+        serverId=$(openstack server list -f value | grep $serverHostname | grep ACTIVE | awk -F ' ' '{print $1}')
+        if [[ $serverId == '' ]]; then
+            echo "Waiting for status to become active"
+            sleep 1
+        fi
+    done
+
+    echo "Server spawned"
+
+    echo "Creating floating IP"
+    floatingIp=$(openstack floating ip create ext-net | grep floating_ip_address | awk -F ' ' '{print $4}')
+    echo "Setting floating IP"
+    openstack server add floating ip $serverHostname $floatingIp
+
+    echo $floatingIp >> servers
+    echo "Server $serverHostname ($floatingIp) configured"
+done <"hostnames"
+
+echo "IP list:"
+cat servers
