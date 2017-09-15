861b9c9a345a4c0cd618edd93af98add894d818e
martinmine <martin_mine@hotmail.com>
2017-09-15 10:24:50 +0200

    Use explicit username/server

--- configure-servers.sh
+++ configure-servers.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Updates and installs docker on all the servers
+
 while IFS= read -r serverEntry
 do
     serverName=$(echo $serverEntry | awk '{print $1}')
     serverIp=$(echo $serverEntry | awk '{print $2}')
-
-    scp install-docker.sh $serverName:.
-    ssh $serverName ./install-docker.sh
+    
+    echo "Configuring server $serverName"
+    scp install-docker.sh centos@$serverIp:.
+    $(ssh centos@$serverIp ./install-docker.sh)
 
 done <"serverlist"
