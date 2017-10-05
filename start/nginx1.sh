5c31d6d2f27062ff329f737e3c37be31f4c4ef2a
martinmine <martin_mine@hotmail.com>
2017-10-05 13:38:53 +0200

    Have same amount of proxies as coap

--- start/nginx1.sh
+++ start/nginx1.sh
@@ -6,4 +6,6 @@ if [[ "$1" != "fast" ]]; then
     docker pull martinmine/acmeairjava_nginx1
 fi 
 
-docker run -d -p 80:80 --name nginx1 martinmine/acmeairjava_nginx1
+docker network create --driver bridge local-net
+docker run -d --network="local-net" -p 8080:80 --name nginx1 martinmine/acmeairjava_nginx1
+docker run -d --network="local-net" -p 80:80 --name proxy martinmine/nginx-adapter 80 nginx1
--- start/support_service_liberty1.sh
+++ start/support_service_liberty1.sh
@@ -8,5 +8,6 @@ if [[ "$1" != "fast" ]]; then
     docker pull martinmine/nginx-adapter
 fi
 
+docker network create --driver bridge local-net
 docker run -d --network="local-net" -p 8080:80 --name support_service_liberty1 martinmine/acmeairjava_support_service_liberty1
 docker run -d --network="local-net" -p 80:80 --name proxy martinmine/nginx-adapter 80 support_service_liberty1
