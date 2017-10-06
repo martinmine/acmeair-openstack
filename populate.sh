9e874a164320a34935e8f09089e99ae9807f577c
martinmine <martin_mine@hotmail.com>
2017-10-06 12:22:39 +0200

    Set -e

--- populate.sh
+++ populate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 curl http://localhost:9000/auth/acmeair-as/rest/api/login/loader/load; echo
 curl http://localhost:9000/booking/acmeair-bs/rest/api/bookings/loader/load; echo
 curl http://localhost:9000/customer/acmeair-cs/rest/api/customer/loader/load?numCustomers=200; echo
